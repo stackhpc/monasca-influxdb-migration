727c891a59a3ac5d2db2346839f90cf9efbab784
Bharat Kunwar <brtknr@bath.edu>
2019-08-21 18:05:15 +0000

    Verne changes

--- migrate.py
+++ migrate.py
@@ -2,11 +2,10 @@
 from helper import MigrationHelper, two_weeks
 
 if __name__ == "__main__":
-    helper = MigrationHelper(source_db='monasca', host='192.168.7.1', verbosity=0)
+    helper = MigrationHelper(source_db='monasca', host='10.114.125.10', verbosity=0)
     helper.migrate(target_db='monasca', db_per_tenant=True,
                    project_defaults={
-                    "03cc1b94463c44e791222cb82008b8b1": dict(end=2, rp=two_weeks), # alaska-prod
-                    "703c23c3219748d484f5d863e7896317": dict(end=2, rp=two_weeks), # alaska-alt-1
+                    "a7c73dd77aac4152b41a7a8fc15c82aa": dict(end=2, rp=two_weeks), # verne-candidate
                    },
                    measurements_file='migrate-measurements.log',
                    success_file='migrate-success.log',
